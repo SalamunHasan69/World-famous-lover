01fd838e32c75903983399a7ba12d40e8a8a53d6
SalamunHasan69e <salamun2600@gmail.com>
2022-08-29 14:30:04 +0600

    second commit

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ const displayUsers = users => {
     userDiv.innerHTML = `
     <img src="${user.picture.large}">
     <h3>Name: ${user.name.title} ${user.name.first} ${user.name.last}</h3>
-    <h5>Age: ${user.dob.age}</h5>
+    
     <p>Location: ${user.location.city}, ${user.location.country}</p>
     <p>E-mail: ${user.email}</p>
     <p>Number: ${user.phone}</p>
@@ -21,3 +21,4 @@ const displayUsers = users => {
   }
 }
 loadUsers();
+{/* <h5>Age: ${user.dob.age}</h5> */ }
